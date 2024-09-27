17c8895b3bb814086fca74b82771de85438949ac
JG-e <zezhengh@163.com>
2024-09-27 13:46:10 -0400

    Refactor raycolor.cpp to include reflection point calculation and implement recursive ray tracing

--- src/raycolor.cpp
+++ src/raycolor.cpp
@@ -3,6 +3,9 @@
 #include "blinn_phong_shading.h"
 #include "reflect.h"
 
+using namespace std;
+using namespace Eigen;
+
 bool raycolor(
   const Ray & ray, 
   const double min_t,
@@ -12,8 +15,25 @@ bool raycolor(
   Eigen::Vector3d & rgb)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  rgb = Eigen::Vector3d(0,0,0);
+  if (num_recursive_calls > 5) {
+    return false;
+  }
+  int hit_id;
+  double t;
+  Vector3d n;
+  if (first_hit(ray, min_t, objects, hit_id, t, n)) {
+    Vector3d color = blinn_phong_shading(ray, hit_id, t, n, objects, lights);
+    Vector3d hit = ray.origin + t * ray.direction;
+    hit += 1e-9 * n;
+    Ray mirror_ray(hit, reflect(ray.direction, n));
+    Vector3d mirror_color;
+    if (raycolor(mirror_ray, 1.0, objects, lights, num_recursive_calls + 1, mirror_color)) {
+      rgb = color + objects[hit_id]->material->km * mirror_color;
+    } else {
+      rgb = color;
+    }
+    return true;
+  }
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }
