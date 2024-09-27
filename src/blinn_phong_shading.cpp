1f8e2de6be65971112d76e663bdc24aa3f4b5afb
JG-e <zezhengh@163.com>
2024-09-27 13:44:05 -0400

    Refactor blinn_phong_shading.cpp to include reflection point calculation and implement Blinn-Phong shading model

--- src/blinn_phong_shading.cpp
+++ src/blinn_phong_shading.cpp
@@ -15,8 +15,37 @@ Eigen::Vector3d blinn_phong_shading(
   const std::vector<std::shared_ptr<Light> > & lights)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Reflection Point 
-  Vector3d reflection_point = ray.origin + t * ray.direction;
-  
+  // Reflection Point p
+  Vector3d p = ray.origin + t * ray.direction;
+  Vector3d L = Vector3d::Zero();
+  for (shared_ptr<Light> light : lights) {
+    // Check if we are in shadow
+    Vector3d light_direction;
+    double max_t;
+    light->direction(p, light_direction, max_t);
+    Ray shadow_ray;
+    shadow_ray.origin = p;
+    shadow_ray.direction = light_direction;
+    int shadow_hit_id;
+    double shadow_t;
+    Vector3d shadow_n;
+    if (first_hit(shadow_ray, 0.0001, objects, shadow_hit_id, shadow_t, shadow_n)) {
+      if (shadow_t < max_t) {
+        // If we are in shadow, skip this light source
+        continue;
+      }
+    }
+    // Calculate Blinn-Phong shading Halfway vector
+    Vector3d v = -ray.direction.normalized();
+    Vector3d l = light_direction.normalized();
+    Vector3d h = (l + v).normalized();
+
+    Vector3d diffuse = objects[hit_id]->material->kd.cwiseProduct(light->I) * max(0.0, n.dot(light_direction));
+    Vector3d specular = objects[hit_id]->material->ks.cwiseProduct(light->I) * pow(max(0.0, n.dot(h)), objects[hit_id]->material->phong_exponent);
+    L += diffuse + specular;
+  }
+
+  Vector3d ambient = 0.1 * objects[hit_id]->material->ka;
+  return ambient + L;
   ////////////////////////////////////////////////////////////////////////////
 }
