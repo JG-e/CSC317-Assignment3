25cc9741f7bbb39ddec42175c7a40a4f16946152
JG-e <zezhengh@163.com>
2024-09-29 16:31:34 -0400

    Refactor blinn_phong_shading.cpp to include shadow ray calculation for Blinn-Phong shading model

--- src/blinn_phong_shading.cpp
+++ src/blinn_phong_shading.cpp
@@ -18,18 +18,18 @@ Eigen::Vector3d blinn_phong_shading(
   // Reflection Point p
   Vector3d p = ray.origin + t * ray.direction;
   Vector3d L = Vector3d::Zero();
+  Ray shadow_ray;
+  shadow_ray.origin = p + 1e-6 * n;
+  double max_t;
   for (shared_ptr<Light> light : lights) {
     // Check if we are in shadow
-    Vector3d light_direction;
-    double max_t;
-    light->direction(p, light_direction, max_t);
-    Ray shadow_ray;
-    shadow_ray.origin = p + 1e-9 * n;
-    shadow_ray.direction = light_direction;
+    light->direction(shadow_ray.origin, shadow_ray.direction, max_t);
+
     int shadow_hit_id;
     double shadow_t;
     Vector3d shadow_n;
-    if (first_hit(shadow_ray, 0.0001, objects, shadow_hit_id, shadow_t, shadow_n)) {
+
+    if (first_hit(shadow_ray, 1e-6, objects, shadow_hit_id, shadow_t, shadow_n)) {
       if (shadow_t < max_t) {
         // If we are in shadow, skip this light source
         continue;
@@ -37,15 +37,16 @@ Eigen::Vector3d blinn_phong_shading(
     }
     // Calculate Blinn-Phong shading Halfway vector
     Vector3d v = -ray.direction.normalized();
-    Vector3d l = light_direction.normalized();
+    Vector3d l = shadow_ray.direction.normalized();
     Vector3d h = (l + v).normalized();
 
-    Vector3d diffuse = objects[hit_id]->material->kd.cwiseProduct(light->I) * max(0.0, n.dot(light_direction));
-    Vector3d specular = objects[hit_id]->material->ks.cwiseProduct(light->I) * pow(max(0.0, n.dot(h)), objects[hit_id]->material->phong_exponent);
+    Vector3d diffuse = (objects[hit_id]->material->kd.array() * (light->I).array()).matrix() * max(0.0, n.dot(shadow_ray.direction));
+    Vector3d specular = (objects[hit_id]->material->ks.array() * (light->I).array()).matrix() * pow(max(0.0, n.dot(h)), objects[hit_id]->material->phong_exponent);
     L += diffuse + specular;
   }
 
   Vector3d ambient = 0.1 * objects[hit_id]->material->ka;
-  return ambient + L;
+  L += ambient;
+  return L;
   ////////////////////////////////////////////////////////////////////////////
 }
